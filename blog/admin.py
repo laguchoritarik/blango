554cf552a6faefeb27a61ab363d6fa6f90d1e1e8
TARIK LAGUCHORI <tarik.lagu@gmail.com>
2026-01-29 23:15:45 +0000

    Finish generic relationships

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,9 @@ from blog.models import Tag, Post
 # Register your models here.
 admin.site.register(Tag)
 class PostAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("title",)}
+    #prepopulated_fields = {"slug": ("title",)}
     list_display = ('slug', 'published_at')
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
+admin.site.register(Tag)
+
